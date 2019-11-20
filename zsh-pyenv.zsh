fcae262445855a327e68c0d94e6abec3f18debc8
@slovacus <slovacus@gmail.com>
2019-11-20 12:08:59 -0500

    refactor: implement functions (#18)

--- zsh-pyenv.zsh
+++ zsh-pyenv.zsh
@@ -8,31 +8,43 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-LIGHT_GREEN='\033[1;32m'
-CLEAR='\033[0m'
+package_name=pyenv
+
+plugin_dir=$(dirname "${0}":A)
+
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/messages.zsh
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/tools.zsh
 
 function pyenv::dependences {
-    echo -e "${CLEAR}${LIGHT_GREEN}Installing Dependences${CLEAR}"
+    message_info "Installing Dependences ${package_name}"
 }
 
 function pyenv::install {
     pyenv::dependences
-    echo -e "${CLEAR}${LIGHT_GREEN} Install pyenv ${CLEAR}"
+    message_info "Installing ${package_name}"
     curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
     pyenv install 3.7.4
     pyenv install 3.8.0
     pyenv global 3.7.4
+    message_success "Installed ${package_name}"
     pyenv::post_install
 }
 
 function pyenv::post_install {
-    echo -e "${CLEAR}${LIGHT_GREEN} Post Install for Pyenv ${CLEAR}"
-    pip install pipenv mypy autopep8 flake8 elpy jedi rope isort epc importmagic yapf pylint cookiecutter
+    message_info "Installing other tools for ${package_name}"
+    pip install --user pipenv mypy autopep8 \
+        flake8 elpy jedi rope \
+        isort epc importmagic \
+        yapf pylint cookiecutter
+    message_success "Success install other tools for ${package_name}"
 }
 
 function pyenv::load {
-    [[ -e "$HOME/.pyenv" ]] && export PYENV_ROOT="$HOME/.pyenv"
-    [[ -e "$HOME/.pyenv/bin" ]] && export PATH="$PATH:$HOME/.pyenv/bin"
+    PATH=$(get_path)
+    [ -e "$HOME/.pyenv" ] && export PYENV_ROOT="$HOME/.pyenv"
+    [ -e "$HOME/.pyenv/bin" ] && export PATH="$PATH:$HOME/.pyenv/bin"
     export PYENV_VIRTUALENV_DISABLE_PROMPT=1
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
@@ -40,6 +52,6 @@ function pyenv::load {
 
 pyenv::load
 
-if [[ ! -x "$(command which pyenv)" ]]; then
+if [ ! -x "$(command which pyenv)" ]; then
     pyenv::install
 fi
