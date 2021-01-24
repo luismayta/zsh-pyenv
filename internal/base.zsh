d6c1af12b99c1d30ea5d8c6fa56bfd2a1a5c9dff
@slovacus <slovacus@gmail.com>
2021-01-24 04:35:42 -0500

    refactor: change code for functions (#43)

---
+++ internal/base.zsh
@@ -0,0 +1,67 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function pyenv::internal::pyenv::install {
+    message_info "Installing ${PYENV_PACKAGE_NAME}"
+    git clone https://github.com/syndbg/pyenv.git ~/.pyenv
+    message_success "Installed ${PYENV_PACKAGE_NAME}"
+}
+
+function pyenv::internal::pyenv::load {
+    [ -e "${PYENV_ROOT_BIN}" ] && export PATH="${PATH}:${PYENV_ROOT_BIN}"
+
+    if type -p pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
+    fi
+}
+
+function pyenv::internal::version::all::install {
+    if ! type -p pyenv > /dev/null; then
+        message_warning "not found pyenv"
+        return
+    fi
+
+    for version in "${PYENV_VERSIONS[@]}"; do
+        message_info "Install version of go ${version}"
+        pyenv install "${version}"
+        message_success "Installed version of go ${version}"
+    done
+    pyenv global "${PYENV_VERSION_GLOBAL}"
+    message_success "Installed versions of Go"
+
+}
+
+function pyenv::internal::version::global::install {
+    if ! type -p pyenv > /dev/null; then
+        message_warning "not found pyenv"
+        return
+    fi
+    message_info "Installing version global of python ${PYENV_VERSION_GLOBAL}"
+    pyenv install "${PYENV_VERSION_GLOBAL}"
+    pyenv global "${PYENV_VERSION_GLOBAL}"
+    message_success "Installed version global of python ${PYENV_VERSION_GLOBAL}"
+}
+
+function pyenv::internal::curl::install {
+    message_info "Installing curl for ${PYENV_PACKAGE_NAME}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${PYENV_MESSAGE_BREW}"
+    fi
+    brew install curl
+    message_success "Installed curl for ${PYENV_PACKAGE_NAME}"
+}
+
+function pyenv::internal::modules::install {
+    if ! type -p python > /dev/null; then
+        message_warning "it's neccesary have python"
+        return
+    fi
+
+    message_info "Installing required python modules"
+    python -m pip install --user --upgrade pip
+    for module in "${PYENV_MODULES[@]}"; do
+        python -m pip install --user --upgrade "${module}"
+    done
+    message_success "Installed required python modules"
+}
--- pkg/main.zsh
+++ pkg/main.zsh
@@ -1,21 +1,25 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-function pkg::main::factory {
+function pyenv::pkg::main::factory {
     # shellcheck source=/dev/null
-    source "${ZSH_TMUX_PATH}"/pkg/base.zsh
+    source "${ZSH_PYENV_PATH}"/pkg/base.zsh
     case "${OSTYPE}" in
     darwin*)
         # shellcheck source=/dev/null
-        source "${ZSH_TMUX_PATH}"/pkg/osx.zsh
+        source "${ZSH_PYENV_PATH}"/pkg/osx.zsh
         ;;
     linux*)
         # shellcheck source=/dev/null
-        source "${ZSH_TMUX_PATH}"/pkg/linux.zsh
+        source "${ZSH_PYENV_PATH}"/pkg/linux.zsh
       ;;
     esac
+
     # shellcheck source=/dev/null
-    source "${ZSH_TMUX_PATH}"/pkg/helper.zsh
+    source "${ZSH_PYENV_PATH}"/pkg/helper.zsh
+
+    # shellcheck source=/dev/null
+    source "${ZSH_PYENV_PATH}"/pkg/alias.zsh
 }
 
-pkg::main::factory
+pyenv::pkg::main::factory
