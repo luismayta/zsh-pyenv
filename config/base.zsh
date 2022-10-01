336f8a13985c36e5ebab46acf7fc844afd4455a5
Luis Mayta <luis@hadenlabs.com>
2022-09-30 22:39:46 -0300

    chore: implement package opencv (#299)

--- config/base.zsh
+++ config/base.zsh
@@ -7,20 +7,21 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export PYENV_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export PYENV_PACKAGE_NAME=pyenv
 export PYENV_VERSIONS=(
-    3.7.4
     anaconda3-5.3.1
     miniconda3-4.3.30
     3.8.0
     3.8.5
     3.9.0
+    3.10.4
 )
-export PYENV_VERSION_GLOBAL=3.8.0
+export PYENV_VERSION_GLOBAL=3.10.4
 export PYENV_MODULES=(
     lastversion
     ansible
     ansible-lint
     checkov
     pip
+    opencv-python
 )
 
 export ZSH_PYENV_LAZY_VIRTUALENV=true
