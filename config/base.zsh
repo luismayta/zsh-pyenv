286251795d182f5ac570400cba98a50de128ee44
Luis Mayta <luis@hadenlabs.com>
2023-03-12 15:20:02 -0300

    chore: implement packages for poetry (#390)

--- config/base.zsh
+++ config/base.zsh
@@ -4,17 +4,14 @@
 export PYENV_ROOT="${HOME}/.pyenv"
 export PYENV_ROOT_BIN="${HOME}/.pyenv/bin"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-export PYENV_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export PYENV_PACKAGE_NAME=pyenv
 export PYENV_VERSIONS=(
     anaconda3-5.3.1
     miniconda3-4.3.30
-    3.8.0
-    3.8.5
-    3.9.0
     3.10.4
+    3.10.6
 )
-export PYENV_VERSION_GLOBAL=3.10.4
+export PYENV_VERSION_GLOBAL=3.10.6
 export PYENV_MODULES=(
     lastversion
     ansible
@@ -23,6 +20,11 @@ export PYENV_MODULES=(
     pip
     pyright
     opencv-python
+    pycodestyle
+    pylint
+    beautysh
+    poetry
+    poetry-poetry-migrate
 )
 
 export ZSH_PYENV_LAZY_VIRTUALENV=true
